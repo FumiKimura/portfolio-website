42da38f57263b7deb41681febdf536387d27a217
Norifumi Kimura <norical496@gmail.com>
2021-08-21 14:25:50 +0900

    proof of cocept is done. That is to have carousel animation

---
+++ script.js
@@ -0,0 +1,38 @@
+const carousel = document.querySelector('.carousel');
+const faces = carousel.querySelectorAll('.carousel__face');
+let selectedIndex = 0;
+let carouselNum = 5;
+let width = window.innerWidth;
+let eachCarouselAngle = 360 / carouselNum;
+let radius = Math.round((210 / 2) / Math.tan(Math.PI / carouselNum));
+
+function rotateCarousel() {
+    let angle = eachCarouselAngle * selectedIndex * -1;
+    carousel.style.transform = 'translateZ(' + radius + 'px) ' + 'rotateY' + '(' + angle + 'deg)';
+}
+
+function initialSetup() {
+    for (let i = 0; i < faces.length; i++) {
+        let face = faces[i];
+        if (i < carouselNum) {
+          let faceAngle = eachCarouselAngle * i;
+          face.style.transform = 'rotateY' + '(' + faceAngle + 'deg) translateZ(' + radius + 'px)';
+        }
+    }
+}
+  
+const rightBtn = document.querySelector('.right-btn');
+rightBtn.addEventListener('click', function() {
+    selectedIndex++;
+    rotateCarousel();
+});
+  
+const leftBtn = document.querySelector('.left-btn');
+leftBtn.addEventListener('click', function() {
+    selectedIndex--;
+    rotateCarousel();
+});
+
+window.addEventListener('load', (event) => {
+    initialSetup();
+});

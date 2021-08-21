1b4d96c2e66462a9137ae179d8742ecf6cb2994a
Norifumi Kimura <norical496@gmail.com>
2021-08-21 22:35:00 +0900

    Fixed awkward move happening between inital load and after first click to rotate

--- script.js
+++ script.js
@@ -2,23 +2,29 @@ const carousel = document.querySelector('.carousel');
 const faces = carousel.querySelectorAll('.carousel__face');
 let selectedIndex = 0;
 let carouselNum = 5;
-let width = window.innerWidth;
 let eachCarouselAngle = 360 / carouselNum;
 let radius = Math.round((210 / 2) / Math.tan(Math.PI / carouselNum));
 
-function rotateCarousel() {
-    let angle = eachCarouselAngle * selectedIndex * -1;
-    carousel.style.transform = 'translateZ(' + radius + 'px) ' + 'rotateY' + '(' + angle + 'deg)';
+function calculateWidth(){
+    // return appropriate width
 }
 
-function initialSetup() {
+function rotateCarousel(){
+    let angle = eachCarouselAngle * selectedIndex * -1;
+    carousel.style.transform = 'translateZ(' + radius + 'px) ' 
+    + 'rotateY' + '(' + angle + 'deg)';
+}
+
+function initialSetup(){
     for (let i = 0; i < faces.length; i++) {
         let face = faces[i];
         if (i < carouselNum) {
           let faceAngle = eachCarouselAngle * i;
-          face.style.transform = 'rotateY' + '(' + faceAngle + 'deg) translateZ(' + radius + 'px)';
+          face.style.transform = 'rotateY' + '(' + faceAngle 
+          + 'deg) translateZ(' + radius + 'px)';
         }
     }
+    rotateCarousel();
 }
   
 const rightBtn = document.querySelector('.right-btn');
